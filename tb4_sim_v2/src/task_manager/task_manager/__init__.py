674f12dd80975b44b0e50ca5e63623f8c45f7968
BInh62153 <sauteej@gmail.com>
2026-07-02 08:56:32 +0700

    Add performance tuning and render engine controls

--- tb4_sim_v2/src/task_manager/task_manager/__init__.py
+++
@@ -1,8 +0,0 @@
-from .state.states import SystemState
-from .state.state_machine import StateMachine, Event
-
-try:
-    from .lifecycle_node import TurtleBot4LifecycleNode, main
-except ImportError:
-    TurtleBot4LifecycleNode = None  # type: ignore[misc, assignment]
-    main = None  # type: ignore[misc, assignment]
